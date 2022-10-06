ca86dc5fe1a34492f92cd43dfb3ce8ba2e2aaf7c
Fred Morris <FredrickCPMorris@hotmail.com>
2022-10-06 11:57:50 -0500

    Create Paths for view functions within the urls file in products.

--- products/views.py
+++ products/views.py
@@ -1,10 +1,7 @@
-from itertools import product
 from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
-
-import products
 from .serializers import ProductSerializer
 from .models import Product
 from products import serializers
