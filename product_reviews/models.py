c42c2486fec628ff6d44d573645ab75a6f30db29
Fred Morris <FredrickCPMorris@hotmail.com>
2022-10-07 09:49:55 -0500

    Fixed typo in settings for urls + Created urls file for product reviews, added Protect arguemtn for models in product foreign key.

--- product_reviews/models.py
+++ product_reviews/models.py
@@ -1,11 +1,12 @@
+from email.policy import default
 from itertools import product
 from unicodedata import name
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 
 class Review(models.Model):
     review_text = models.CharField(max_length=255)
     rating = models.IntegerField()
     name = models.CharField(max_length=255)
-    product = models.ForeignKey()
+    product = models.ForeignKey(User, default=None, on_delete=models.PROTECT)
