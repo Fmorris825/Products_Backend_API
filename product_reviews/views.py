38a430d5d57bd89ed7bbf452ad008cfda0adbcc1
Fred Morris <FredrickCPMorris@hotmail.com>
2022-10-07 09:37:52 -0500

    Created CRUD functionality in views file for Reviews class

--- product_reviews/views.py
+++ product_reviews/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from configparser import InterpolationMissingOptionError
+from django.shortcuts import get_object_or_404
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import ReviewSerializer
+from .models import Review
+from product_reviews import serializers
 
 # Create your views here.
+
+@api_view(['GET', 'POST'])
+def review_list(request):
+    
+    if request.method == 'GET':
+        reviews = Review.objects.all()
+        serializer = ReviewSerializer(reviews, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = Review(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.errors, status=status.HTTP_201_CREATED)
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def review_detail(request, pk):
+    review = get_object_or_404(ReviewSerializer, pk=pk)
+    if request.method == 'GET':
+        serializer = ReviewSerializer(review)
+        return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
+    elif request.method == 'PUT':
+        serializer = ReviewSerializer(review, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
+    elif request.method == 'DELETE':
+        review.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
