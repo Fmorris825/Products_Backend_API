d3dc9d41b8f7e352f086a8762a85e84a4770ebb5
Fred Morris <FredrickCPMorris@hotmail.com>
2022-10-11 11:08:12 -0500

    Minor adjustment to models

--- product_reviews/models.py
+++ product_reviews/models.py
@@ -1,7 +1,3 @@
-from email.policy import default
-from itertools import product
-from turtle import title
-from unicodedata import name
 from django.db import models
 from products.models import Product
 # Create your models here.
